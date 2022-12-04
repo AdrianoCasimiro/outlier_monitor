38e7046a87a5e9b1a83eca00b5a549815403929a
Adriano Casimiro <acasimiro@bcfsolutions.net.br>
2022-12-04 16:42:29 -0300

    tests added

--- api/core/tests.py
+++ api/core/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+CREATE_OUTLIER_URL = reverse('create_outliers')
+class TestOutlier(TestCase):
+    '''Create a new Outlier'''
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_outlier_success(self):
+        '''Test creating a new Outlier'''
+
+        payload = {
+            'project_name': 'RaizenOutlier',
+            'table_name': 'Values',
+            'outlier': '1 0.6'
+        }
+
+        res = self.client.post(CREATE_OUTLIER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    
+    
+class ListOutlier(TestCase):
+    '''List Outliers registered'''
+
+    def setUp(self):
+        self.client = APIClient()
+        payload = {
+            'project_name': 'RaizenOutlier',
+            'table_name': 'Values',
+            'outlier': '1 0.6'
+        }
+
+        self.client.post(CREATE_OUTLIER_URL, payload)
+
+    def test_create_outlier_success(self):
+        '''Test Listining Outliers'''
+
+        res = self.client.get('/api/list_outliers/RaizenOutlier/Values/')
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- api/core/urls.py
+++ api/core/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 app_name = 'core'
 
 urlpatterns = [
-    path('api/create_outlier/', CreateOutlierProbabilityViewset.as_view(), name='outliers'),
+    path('api/create_outlier/', CreateOutlierProbabilityViewset.as_view(), name='create_outliers'),
     path('api/list_outliers/<str:project_name>/<str:table_name>/', ListOutliersProbabilityViewset.as_view(), name='outliers'),
     ]
